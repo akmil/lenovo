341f74fc631644cd60e743674978ee86d9f2a4be
pashaOliinyk <pashades@gmail.com>
2016-08-23 18:25:52 +0300

    fix: img fix: affix value on resize mod: preloader to JS

--- public/scripts/components/preloader.js
+++ public/scripts/components/preloader.js
@@ -1,7 +1,16 @@
+/*
 $(window).load(function() { // makes sure the whole site is loaded
   $('#preloader').fadeOut(); // will first fade out the loading animation
-  $('#status').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('#status').fadeOut('slow'); // will fade out the white DIV that covers the website.
   $('body').delay(350).css({
     'overflow': 'visible'
   });
 });
+*/
+
+window.onload = hidePreloader;
+function hidePreloader() {
+    document.querySelector("#status").style.display = 'none';
+    // document.querySelector("#preloader").style.display = 'none';
+    document.querySelector("body").style.overflow = 'visible';
+}
